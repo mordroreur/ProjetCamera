6760ae81c33994a88eb20857c61eeab2608a6dfe
mordroreur <mlaurencot@gmail.com>
2024-02-06 14:19:51 +0100

    master

---
+++ detectionContour.py
@@ -0,0 +1,37 @@
+#import opencv librairy
+import cv2 
+import numpy as np  
+  
+# define a video capture object 
+vid = cv2.VideoCapture(0, cv2.CAP_DSHOW) 
+
+if(not vid.isOpened()):
+    print("Erreur d'ouverture de la caméra!")
+    exit()
+ret, frame = vid.read()
+height, width = frame.shape[:2]
+
+
+print("Pour sortir de l'application appuyez sur 'Q'.")
+
+while(True): 
+      
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read() 
+
+    #cv2.rectangle(frame,(384,0),(510,128),(0,255,0),3)
+    
+    # Display the resulting frame 
+    cv2.imshow('frame', cv2.Canny(frame, 100, 150)) 
+      
+    # the 'q' button is set as the 
+    # quitting button you may use any 
+    # desired button of your choice 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+  
+# After the loop release the cap object 
+vid.release()
+# Destroy all the windows 
+cv2.destroyAllWindows() 
