3082454ddad545704991e8c9d88579cf7326ae3c
mordroreur <mlaurencot@gmail.com>
2024-02-06 16:47:16 +0100

    add new methode

---
+++ mathTest.py
@@ -0,0 +1,63 @@
+# import the necessary packages
+import numpy as np
+import cv2
+
+# disable the input
+output = False
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+
+# open webcam video stream
+cap = cv2.VideoCapture(0)
+
+
+# the output will be written to output.avi
+if(output):
+    out = cv2.VideoWriter( 'output.avi', cv2.VideoWriter_fourcc(*'MJPG'), 15.,(640,480))
+
+
+while(True):
+    # Capture frame-by-frame the camera
+    ret, frame = cap.read()
+
+    # resizing for faster detection
+    frame = cv2.resize(frame, (640, 480))
+
+    # using a greyscale picture, also for faster detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # detect all potential people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(4, 4),
+                                            padding=(4, 4),
+                                            scale=1.05)
+
+    for b in boxes:
+        print(b)
+    #boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
+    
+    # Write the output video
+    if(output):
+        out.write(frame.astype('uint8'))
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+# and release the output
+if(output):
+    out.release()
+# finally, close the window
+cv2.destroyAllWindows()
+cv2.waitKey(1)
